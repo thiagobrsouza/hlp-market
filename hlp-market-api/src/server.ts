e754ab7902fbe6fc5e674ef41e1d0adb8f9acba5
Thiago Souza <tbs.souza@outlook.com>
2025-11-21 19:12:21 -0300

    Projeto criado

---
+++ hlp-market-api/src/server.ts
@@ -0,0 +1,14 @@
+import 'express-async-errors';
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(express.json());
+
+app.use(morgan('dev'));
+
+const port = 3001;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
